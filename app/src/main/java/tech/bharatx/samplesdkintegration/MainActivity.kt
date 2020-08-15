825e23341c7d424ccf73f11c56ecbd7d7da1b725
Siddharth Venu <siddharthvvenu@gmail.com>
2020-08-15 19:55:33 +0530

    feat: update SDK usage to new flow

--- app/src/main/java/tech/bharatx/samplesdkintegration/MainActivity.kt
+++ app/src/main/java/tech/bharatx/samplesdkintegration/MainActivity.kt
@@ -6,10 +6,9 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import tech.bharatx.alternatedata.AlternateDataManager
 import tech.bharatx.common.BharatXCommonUtilManager
 import tech.bharatx.creditaccess.CreditAccessManager
-import tech.bharatx.securityhelpers.SecurityStorageManager
+import tech.bharatx.startup.BharatXStartupTierManager
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,18 +17,20 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
 
     // testing credentials
-    SecurityStorageManager.storePartnerId(this@MainActivity, "bharatx")
-    SecurityStorageManager.storePartnerApiKey(this@MainActivity, "testKey")
+    BharatXStartupTierManager.initialize(
+        this,
+        "testPartnerId",
+        "testApiKey",
+        Color.parseColor("#4892CB") // This is the same color as BharatX's default theme color
+    )
 
     // enable BharatX credit notifications
     CreditAccessManager.register(this@MainActivity)
 
-    // This is the same color as BharatX's default theme color
-    SecurityStorageManager.storeThemeColorPreference(this@MainActivity, Color.parseColor("#4892CB"))
-
     // confirm whether user wants to go ahead with the transaction
     BharatXCommonUtilManager.confirmTransactionWithUser(
         this,
+        10000, // ask user for confirmation for 100 rupees
         object : BharatXCommonUtilManager.TransactionConfirmationListener {
 
           override fun onUserConfirmedTransaction() {
@@ -40,68 +41,38 @@ class MainActivity : AppCompatActivity() {
           override fun onUserCancelledTransaction() {
             Toast.makeText(this@MainActivity, "Transaction cancelled", Toast.LENGTH_LONG).show()
           }
-
-          override fun onUserAcceptedPrivacyPolicy() {
-            // request/register for Alternate Data collection once user has accepted the privacy
-            // policy
-            if (AlternateDataManager.areAllAlternateDataPermissionsGranted(this@MainActivity)) {
-              AlternateDataManager.register(this@MainActivity)
-            } else {
-              AlternateDataManager.requestAlternateDataPermissions(this@MainActivity)
-            }
-          }
-
-          override fun onUserDeniedPrivacyPolicy() {
-            Toast.makeText(
-                this@MainActivity,
-                "Please restart the app to accept our privacy policy and continue",
-                Toast.LENGTH_LONG)
-                .show()
-          }
         })
-  }
-
-  override fun onRequestPermissionsResult(
-      requestCode: Int, permissions: Array<out String>, grantResults: IntArray
-  ) {
-    if (requestCode == AlternateDataManager.alternateDataPermissionsRequestCode) {
-      if (AlternateDataManager.areAllAlternateDataPermissionsGranted(this)) {
-        // all permissions granted
-        AlternateDataManager.register(this@MainActivity)
-      } else {
-        Toast.makeText(
-            this,
-            "We need to collect your data to determine your credit limit, please enable permissions in settings",
-            Toast.LENGTH_LONG)
-            .show()
-      }
-    }
   }
 
   private fun startBharatXTransaction() {
-    val transactionId = "123" // test transaction id - obtain from server
-    CreditAccessManager.registerTransactionId(
-        this,
-        transactionId,
-        object : CreditAccessManager.RegisterTransactionListener {
-          override fun onRegistered() {
-            // simulate processing
-            Handler(Looper.getMainLooper())
-                .postDelayed(
-                {
-                  BharatXCommonUtilManager.closeBharatXProgressDialog()
-                  Toast.makeText(
-                      this@MainActivity,
-                      "Thank you for your purchase. A jar of cookies for your web browser will arrive shortly :)",
-                      Toast.LENGTH_LONG)
-                      .show()
-                },
-                60000)
-          }
+    BharatXCommonUtilManager.showBharatXProgressDialog(this)
+    var transactionId = ""
+    Handler(Looper.getMainLooper())
+        .postDelayed(
+        {
+          transactionId = "123" // test transaction id - obtain from server
+          CreditAccessManager.registerTransactionId(
+              this,
+              transactionId,
+              object : CreditAccessManager.RegisterTransactionListener {
+                override fun onRegistered() {
+                  // simulate processing
+                  Handler(Looper.getMainLooper())
+                      .postDelayed(
+                      {
+                        BharatXCommonUtilManager.closeBharatXProgressDialog()
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Thank you for your purchase. A jar of cookies for your web browser will arrive shortly :)",
+                            Toast.LENGTH_LONG)
+                            .show()
+                      },
+                      1000)
+                }
 
-          override fun onUserCancelledTransaction() {}
-
-          override fun onFailure() {}
-        })
+                override fun onFailure() {}
+              })
+        },
+        2000)
   }
 }
